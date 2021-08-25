42b058729fdd24004cf7385eac078b9b535c186d
Daniel Zamanillo <dzamanillok@gmail.com>
2021-08-25 11:33:51 -0500

    seeds added

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,25 @@ CREATE DATABASE staff_service_solutions;
 
 USE staff_service_solutions;
 
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
